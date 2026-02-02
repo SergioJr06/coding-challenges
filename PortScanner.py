becf4fd5ac83f4b3a0303b8511ad55040caaeb74
Sérgio Eduardo <sergioedumusico15@gmail.com>
2026-02-01 23:32:51 -0300

    Fix: Mudança de serial para multithreading

--- PortScanner.py
+++ PortScanner.py
@@ -1,12 +1,35 @@
 import socket
+import threading
+import time
 
-endereco = input("Digite o seu endereço para o PortScanner passar as portas abertas!\n")
-porta_final = input("Até qual porta você quer tenta?\n")
-porta_final = int(porta_final)
-
-for porta in range(1, porta_final + 1):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
-    if s.connect_ex((endereco, porta)) == 0:
-        print(f"A porta {porta} está aberta!")  
-    s.close()
+thread_limit = threading.Semaphore(value=100) #define o valor limite de thread para rodar ao mesmo tempo. (concorrencia controlada)
+    
+def scan_port(endereco, porta):
+    with thread_limit:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        if s.connect_ex((endereco, porta)) == 0:
+            print(f"[+] Porta {porta} aberta!")  
+        s.close()
+     
+if __name__ == "__main__": #este name main significa para rodar o programa daq para baixo se for executado
+    endereco = input("Insira o Endereço: ")
+    porta_final = int(input("Insira uma porta limite: "))
+    
+    tempo_inicial = time.time()
+    
+    threads = []
+    for porta in range(1, porta_final + 1):
+        t = threading.Thread(target=scan_port, args=(endereco, porta))
+        threads.append(t)
+        t.start()
+            
+    for t in threads:
+        t.join()
+        
+    tempo_final = time.time()
+    
+    duracao = tempo_final - tempo_inicial
+        
+    print(f"\n[!] Escaneamento concluído em {duracao:.2f} segundos.")
+        

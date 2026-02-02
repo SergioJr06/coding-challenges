7f59e389e4a8d1b89dfff5722795aeafc1d382e4
Sérgio Eduardo <sergioedumusico15@gmail.com>
2026-02-01 22:56:12 -0300

    feat: implementando scanner de portas funcional com python

--- PortScanner.py
+++ PortScanner.py
@@ -1,2 +1,12 @@
 import socket
 
+endereco = input("Digite o seu endereço para o PortScanner passar as portas abertas!\n")
+porta_final = input("Até qual porta você quer tenta?\n")
+porta_final = int(porta_final)
+
+for porta in range(1, porta_final + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    if s.connect_ex((endereco, porta)) == 0:
+        print(f"A porta {porta} está aberta!")  
+    s.close()
